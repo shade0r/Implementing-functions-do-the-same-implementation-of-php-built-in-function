36d0f986bb6998c63562aa7529ee15a5e6612571
Shade0r <Shade0r@proton.me>
2022-12-10 17:50:53 +0200

    First function

---
+++ array_merge.php
@@ -0,0 +1,20 @@
+<?php
+//Implementing function like array_merge()
+$arr1 = array('first', 'second', 'third', 'forth');
+$arr2 = array('The first one', 'second','The second one', 'The third one ', 'The forth one','Another Str');
+function arrayMerge(...$arrays) :array|string
+{
+    $result = [];
+    $count = count($arrays);
+    for ($count = 0; $count< count($arrays) ; $count++) {
+        if (empty($arrays[$count])){
+            return "Array number ".($count+1)." is Empty";
+        }
+        for ($i = 0; $i < count($arrays[$count]); $i++) {
+            $result[] = $arrays[$count][$i];
+        }
+    }
+
+    return $result;
+}
+var_dump(arrayMerge($arr1,$arr2));
