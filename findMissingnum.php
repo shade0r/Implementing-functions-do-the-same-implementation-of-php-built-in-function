20dc63f5068754b1efc7cc3cc65dbb9e78636786
Shade0r <Shade0r@proton.me>
2022-12-11 19:12:53 +0200

    function to echo the first missing number

---
+++ findMissingnum.php
@@ -0,0 +1,14 @@
+<?php
+function findMissnum(array $set) :string {
+    //sort the numbers inside the array
+    $set[] = sort($set);
+    for ($i = 0 ;$i <= count($set); $i++){
+        if ($i >= 1){
+            if (($set[$i]-($set[$i-1])) !=1){
+                return "The first missing number is ".$set[$i]-1;
+                }
+        }
+    }
+    }
+    echo findMissnum(array(1,2,3,5,6));
+//output should be 3
