b256a1ae3ff74c4be1e11ab351d68a390ec02a49
Shade0r <Shade0r@proton.me>
2022-12-11 19:18:24 +0200

    enhance the logic of function

--- findMissingnum.php
+++ findMissingnum.php
@@ -5,10 +5,10 @@ function findMissnum(array $set) :string {
     for ($i = 0 ;$i <= count($set); $i++){
         if ($i >= 1){
             if (($set[$i]-($set[$i-1])) !=1){
-                return "The first missing number is ".$set[$i]-1;
+                return "The first missing number is ".$set[$i-1]+1;
                 }
         }
     }
     }
-    echo findMissnum(array(1,2,3,5,6));
+    echo findMissnum(array(1,2,3,4,7));
 //output should be 3
